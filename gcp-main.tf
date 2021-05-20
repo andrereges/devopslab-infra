6eb80db62f8b42e9138cccaad03aa197c3be6d23
Andrê Reges <dreeh.silva@hotmail.com>
2021-05-20 13:27:42 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,35 +1,5 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "helloworld" {
-  name         = "website"
-  machine_type = "n1-standard-1"
-  zone         = "southamerica-east1-a"
-  tags         = ["labdevops"]
-
-  # Defini a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-bionic-v20210508"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    }
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-  }
-
-}
-
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-cloud-andre"
-  location_id = "us-central"
-}
-
-# Retorna o IP da VM criada
-output "ip" {
-  value = google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip
+  location_id = "us-east1"
 }
 
